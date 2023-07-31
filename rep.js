71bb2d9b097f0bc8928e749100c27efde2702e57
Dayana <dr750837@gmail.com>
2023-07-31 08:55:38 -0500

    ejemplo

---
+++ rep.js
@@ -0,0 +1,6 @@
+let numero1 = parseFloat(prompt("ingrese el primer numero"))
+let numero2 = parseFloat(prompt("ingrese el segundo numero"))
+ 
+let promedio = (numero1 + numero2)/2 
+
+alert ("el promeddio de los numeros es:" + promedio)
